8241a680946122c414c494d3c05a01fa34a45784
adithyanS007 <adhiadithyan21@gmail.com>
2025-03-21 16:00:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// server/index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const foodRoutes = require('./routes/foodRoute.js');
+const drinkRoutes = require('./routes/drinksRoute.js');
+const brunchRoutes = require('./routes/brunchRoutes.js');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // Allow both frontend ports
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  credentials: false
+};
+
+// Use CORS middleware with the specified options
+app.use(cors(corsOptions));
+app.use(bodyParser.json()); // Middleware to parse JSON
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Routes
+app.use('/api/foods', foodRoutes);
+app.use('/api/drinks', drinkRoutes);
+app.use('/api/brunches', brunchRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
